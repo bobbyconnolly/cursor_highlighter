f1a271dd1a5c0fecadda6d1e49a0d2cbe6744516
Thomas Connolly <connollythomas@hotmail.com>
2024-08-25 18:58:50 -0400

    update batch file

--- run_highlighter.bat
+++ run_highlighter.bat
@@ -1,2 +1,18 @@
 @echo off
-"C:\Users\Bobby\PycharmProjects\cursor_highlighter\.venv\Scripts\pythonw.exe" "C:\Users\Bobby\PycharmProjects\cursor_highlighter\main.py"
+start /min "" cmd /c "
+set PROJECT_DIR=C:\Users\Bobby\PycharmProjects\cursor_highlighter
+set MAIN_SCRIPT=%PROJECT_DIR%\main.py
+
+if exist "%PROJECT_DIR%\venv" (
+    set "VENV_DIR=%PROJECT_DIR%\venv"
+) else if exist "%PROJECT_DIR%\.venv" (
+    set "VENV_DIR=%PROJECT_DIR%\.venv"
+) else (
+    echo Virtual environment not found.
+    exit /b 1
+)
+
+start "" "%VENV_DIR%\Scripts\pythonw.exe" "%MAIN_SCRIPT%"
+exit
+"
+
